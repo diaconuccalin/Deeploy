26db64372c0d871c4376b4fbe7a6350b83b3e116
diaco <diaconuccalin@gmail.com>
2026-01-26 14:39:54 +0000

    Extracted layers for model testing

--- Deeploy/Targets/PULPOpen/Parsers.py
+++ Deeploy/Targets/PULPOpen/Parsers.py
@@ -75,11 +75,6 @@ class PULPFPConv2DParser(Conv2DParser):
                 # Current PULP kernel only supports grouping of 1
                 self.operatorRepresentation['group'] == 1,
 
-                # Make sure padding is square
-                self.operatorRepresentation['pads'][0] == self.operatorRepresentation['pads'][2],
-                self.operatorRepresentation['pads'][1] == self.operatorRepresentation['pads'][3],
-                self.operatorRepresentation['pads'][0] == self.operatorRepresentation['pads'][1],
-
                 # Check number of inputs
                 # 2 inputs if no bias, 3 if layer has bias
                 len(node.inputs) in [2, 3],
@@ -133,11 +128,6 @@ class PULPFPDWConv2DParser(Conv2DParser):
         if wellFormed:
             # Check if the node is a depthwise convolution
             ret = all([
-                # Make sure padding is square
-                self.operatorRepresentation['pads'][0] == self.operatorRepresentation['pads'][2],
-                self.operatorRepresentation['pads'][1] == self.operatorRepresentation['pads'][3],
-                self.operatorRepresentation['pads'][0] == self.operatorRepresentation['pads'][1],
-
                 # Check number of inputs
                 # 2 inputs if no bias, 3 if layer has bias
                 len(node.inputs) in [2, 3],
